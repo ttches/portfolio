1766b86085891644cb332c4f36c1ccce19efc31a
Tyler Ches <ttches@gmail.com>
2017-05-27 16:38:52 -0700

    Started adding SVGs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('this worked');
+import './index.css';
+
+document.addEventListener("DOMContentLoaded", function() {
+  let fullHeightElement = document.getElementsByClassName('fullheight');
+
+  function changeHeight() {
+    let wheight = window.innerHeight;
+    for (let i = 0; i < fullHeightElement.length; i++) {
+      fullHeightElement[i].style.height = wheight + "px";
+    }
+  };
+
+  changeHeight();
+
+  window.addEventListener('resize', changeHeight);
+
+});
